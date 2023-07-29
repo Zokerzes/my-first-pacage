c147506ae0cc1db85ebd06d0338cc9a60c20c7c1
Zokerzes <yozhik13@gmail.com>
2023-07-30 02:26:17 +0300

    ascii

--- my-pacage.js
+++ my-pacage.js
@@ -1,9 +1,12 @@
 import { say } from 'cowsay';
 import chalk from 'chalk';
 
+
 const hello = 'Hello, World!';
 const eyes = chalk.blue.bold('oO');
 const tongue = chalk.rgb(255, 51, 153).italic(' U');
+
+
 
 console.log(say({
   text: 'Хто здесь?',
