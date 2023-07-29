42f7a15b256e95526ca9c9f6387fee1109f74d36
Zokerzes <yozhik13@gmail.com>
2023-07-30 02:41:56 +0300

    ++

--- my-pacage.js
+++ my-pacage.js
@@ -1,15 +1,14 @@
 import { say } from 'cowsay';
 import chalk from 'chalk';
-
+import art from "ascii-art";
 
 const hello = 'Hello, World!';
 const eyes = chalk.blue.bold('oO');
 const tongue = chalk.rgb(255, 51, 153).italic(' U');
-
-
+const ar = art.style("Хто здесь?", 'green', true)
 
 console.log(say({
-  text: 'Хто здесь?',
+  text: ar,
   e: eyes,
   T: tongue,
   mode: 'b',
